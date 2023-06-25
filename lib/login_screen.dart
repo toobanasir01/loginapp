d955540b4b26de55f54ce64a3b01032940aff1ee
Tooba Nasir <toobanasir01@gmail.com>
2023-06-25 20:46:36 +0500

    validation app

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,9 +1,10 @@
 import 'package:assignment6/home_screen.dart';
 import 'package:assignment6/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginScreen extends StatefulWidget {
-    const LoginScreen({super.key});
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -26,108 +27,112 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.all(16),
           child: Form(
             key: formKey,
-            child:  Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text
-                  ("Welcome back", style: TextStyle(fontSize: 40),),
-                const Text("Login your account", style: TextStyle(fontSize: 16),),
-                const SizedBox(height: 20,),
-                TextFormField(
-                  controller: nameCtrl,
-                  validator: (String? value){
-                    if(value==null || value.isEmpty){
-                      return "please Filed the field";
-                    }
-                    return null;
-                  },
-
-                  cursorColor: Colors.grey,
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white,
-                      prefixIcon: Icon(Icons.person,color: Colors.grey,),
-                      hintText: "Username",
-                      border: OutlineInputBorder()
-                  ),
-
+                const Text(
+                  "Welcome back",
+                  style: TextStyle(fontSize: 40),
                 ),
-
+                const Text(
+                  "Login your account",
+                  style: TextStyle(fontSize: 16),
+                ),
                 const SizedBox(
                   height: 20,
                 ),
-
                 TextFormField(
-
-                  obscureText: true,
-
+                  controller: nameCtrl,
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "please Filled the field";
+                    }
+                    return null;
+                  },
                   cursorColor: Colors.grey,
                   decoration: const InputDecoration(
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.grey,
+                      ),
+                      hintText: "Username",
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: MultiValidator([
+                    RequiredValidator(errorText: "Password is Required"),
+                    LengthRangeValidator(
+                        min: 5, max: 8, errorText: "Enter Strong Password minimum 5 to 8 words")
+                  ]),
+                  obscureText: true,
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
                       hintText: "Password",
-                      prefixIcon: Icon(Icons.password,color: Colors.grey,),
-                      border: OutlineInputBorder(
-
-                      )
-                  ),
-
+                      prefixIcon: Icon(
+                        Icons.password,
+                        color: Colors.grey,
+                      ),
+                      suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                      border: OutlineInputBorder()),
                 ),
                 const SizedBox(
                   height: 50,
                 ),
-
                 SizedBox(
-                  width: size.width*0.9,
+                  width: size.width * 0.9,
                   height: 50,
-
                   child: ElevatedButton(
-
                       style: ElevatedButton.styleFrom(
                           backgroundColor: const Color(0xff395BA9),
                           shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)
-                          )
-                      ),
-                      onPressed: (){
-                        if(formKey.currentState!.validate()== true){
-                          Navigator.push(context,
+                              borderRadius: BorderRadius.circular(20))),
+                      onPressed: () {
+                        if (formKey.currentState!.validate() == true) {
+                          Navigator.push(
+                              context,
                               MaterialPageRoute(
-                                  builder: (context)=> HomeScreen(
-                                    name: nameCtrl.text,
-
-                                  )));
-
+                                  builder: (context) => HomeScreen(
+                                        name: nameCtrl.text,
+                                      )));
                         }
-
-                      }, child: const Text("Login")),
-
+                      },
+                      child: const Text("Login")),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 Container(
                   margin: const EdgeInsets.all(7),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-
                     children: [
-
                       Container(
-                          margin:const EdgeInsets.only(right: 10),
-                          child: const Text("Don't have Account ?",style: TextStyle(fontSize: 15),)),
+                          margin: const EdgeInsets.only(right: 10),
+                          child: const Text(
+                            "Don't have Account ?",
+                            style: TextStyle(fontSize: 15),
+                          )),
                       GestureDetector(
-                        child: const Text("SignUp",style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
-                        onTap: (){
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context)=> const RegisterScreen(),
-
-
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
                               ));
                         },
                       )
                     ],
                   ),
                 ),
-
-
-
-
               ],
             ),
           ),
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
@@ -11,10 +12,17 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final passwordValidator =
+        MultiValidator([RequiredValidator(errorText: "required"),
+        LengthRangeValidator(min: 5, max: 8, errorText: "Enter Strong paswword min 5 to 8 words")]);
+    var password;
+    GlobalKey<FormState> formKey2 = GlobalKey<FormState>();
+
     return Scaffold(
       backgroundColor: const Color(0xffDAE2FF),
       body: SafeArea(
         child: Form(
+          key: formKey2,
           child: Container(
             width: size.width,
             height: size.height,
@@ -22,59 +30,99 @@ class _RegisterScreenState extends State<RegisterScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Register",style: TextStyle(fontSize: 40),),
-                const SizedBox(height: 20,),
+                const Text(
+                  "Register",
+                  style: TextStyle(fontSize: 40),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   validator: RequiredValidator(errorText: "Required*"),
                   cursorColor: Colors.grey,
                   decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person,color: Colors.grey,),
-                    hintText: "Username",
-                    border: OutlineInputBorder()
-                  ),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.grey,
+                      ),
+
+                      hintText: "Username",
+                      border: OutlineInputBorder()),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
+                  validator: MultiValidator([
+                    EmailValidator(errorText: "enter a valid email address"),
+                    RequiredValidator(errorText: "this fieled is required")
+                  ]),
                   cursorColor: Colors.grey,
                   decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.email,color: Colors.grey,),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.grey,
+                      ),
+
                       hintText: "Email",
-                      border: OutlineInputBorder()
-                  ),
+                      border: OutlineInputBorder()),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
+                  onChanged: (val) => password = val,
+                  validator: passwordValidator,
+                  obscureText: true,
                   cursorColor: Colors.grey,
                   decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.email,color: Colors.grey,),
-                      hintText: "Email",
-                      border: OutlineInputBorder()
-                  ),
+                      prefixIcon: Icon(
+                        Icons.password,
+                        color: Colors.grey,
+                      ),
+                      suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                      hintText: "Password",
+                      border: OutlineInputBorder()),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
+                  validator: (val) =>
+                      MatchValidator(errorText: 'passwords do not match')
+                          .validateMatch(val.toString(), password),
                   cursorColor: Colors.grey,
+                  obscureText: true,
                   decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.email,color: Colors.grey,),
-                      hintText: "Email",
-                      border: OutlineInputBorder()
-                  ),
+                      prefixIcon: Icon(
+                        Icons.password,
+                        color: Colors.grey,
+                      ),
+                      suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                      hintText: "Confirm Password",
+                      border: OutlineInputBorder()),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 SizedBox(
-                    width: size.width*0.9,
+                    width: size.width * 0.9,
                     height: 50,
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          backgroundColor:  const Color(0xff395BA9),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          )
-                        ),
-                        onPressed: (){
-                          Navigator.pop(context);
-
-                        }, child: const Text("Register"))),
+                            backgroundColor: const Color(0xff395BA9),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            )),
+                        onPressed: () {
+                          if (formKey2.currentState!.validate() == true) {
+                            Navigator.pop(context);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text("Please filled all field")));
+                          }
+                        },
+                        child: const Text("Register"))),
                 const SizedBox(
                   height: 10,
                 ),
@@ -82,17 +130,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      padding: const EdgeInsets.only(right: 10,),
-                      child: const Text("Already have an Account",),
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                      ),
+                      child: const Text(
+                        "Already have an Account",
+                      ),
                     ),
                     GestureDetector(
-                      child: const Text("Login",style:  TextStyle(fontWeight: FontWeight.bold),),
-                      onTap:(){
-                      Navigator.pop(context);
-                    } ,)
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    )
                   ],
                 )
-
               ],
             ),
           ),
@@ -101,4 +156,3 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 }
-
